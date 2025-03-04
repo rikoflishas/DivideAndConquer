fcd600c87c95ce7fcdf6ce61e2c546279adacf78
BenjaminArroyo <benji_rikoflishas@hotmail.com>
2025-03-03 21:53:11 -0700

    usando mergesort para hallar el valor mayor en el arreglo

--- js/main.js
+++ js/main.js
@@ -1,30 +1,54 @@
-// let findMax = (arr) => 
-// {
-//     if(arr.length === 1){
-//         return arr.length[0];
-//     }
 
-//  let mid = Math.floor(arr.length / 2); // se redondea al numero menor. 
-//  let maxIzquierda = findMax(arr.slice(0, mid)); // Mitad izquierda 
-//  let maxDerecha = findMax(arr.slice(mid)); // Mitad derecha 
+/**
+ * Proyecto del Divide y Vencerás
+El paradigma Divide and Conquer es un enfoque poderoso para resolver problemas complejos al dividirlos en partes más pequeñas y resolverlos recursivamente.
+ Aunque puede parecer complicado al principio, con la práctica se convierte en una herramienta esencial para diseñar algoritmos eficientes. Es ampliamente utilizado en la vida cotidiana y en aplicaciones técnicas.
 
+Objetivo
+El alumno debe usar "divide and conquer" encontrar el número máximo un arreglo. 
+Practicará cómo dividir un problema en partes más pequeñas y detener la recursión en un caso base (nuevamente).
 
-//  return Math.max(maxIzquierda, maxDerecha); // Combinar resultados };
-// }
+Problema: Búsqueda del Máximo en un Arreglo con Divide and Conquer
+Dado un arreglo de números, implementa una función que utilice el enfoque Divide and Conquer 
+para encontrar el número máximo.
 
-// const numbers = [3, 8, 2, 10, 5, 7];
+Instrucciones para resolver el problema:
+Divide el arreglo en dos mitades.
+Resuelve el problema recursivamente para encontrar el máximo en cada mitad.
+Combina las soluciones comparando los máximos de ambas mitades.
+Devuelve el número máximo encontrado.
+Puedes consultar un extracto de código para comenzar con la solución, 
+se incluye un ejmplo de lo que se espera en la siguiente url:
+ https://gist.github.com/heladio-devf-mx/3b019c2bcdb4354d6b7b51e1e9e48f7f
 
-let findMax = (arr) => 
+ */
+
+function mergeSort(numbers)
 {
-    if(arr.length === 1)    return arr[0];//solo cuando es un arreglo de un dato
-
-    let mid = Math.floor(arr.length / 2);
+    //Usaremos mergeSort para hallar el número más grande del arreglo.
+    if(numbers.length <= 1)    return numbers.length === 0 ? null : numbers[0];
+    //                                  condition ? valueIfTrue : valueIfFalse
     
-    if(arr[mid] < arr[mid - 1]){
-        dato = arr[mid];
-        arr[mid] = arr[mid - 1];
-        arr[mid - 1] = dato;
+
+    let mid = Math.floor(numbers.length / 2);
+    let leftArray = numbers.slice(0, mid);// divide del principio hasta la mitad (no agarra el último elemento)
+    let rightArray = numbers.slice(mid); //desde la mitad al final
+
+    //Conquer. Aplicando recursividad para seguir dividiendo. Conquer se refiere a dividirlo lo que más se pueda
+    const leftMax = mergeSort(leftArray);
+    const rightMax = mergeSort(rightArray);
+
+    //Combinar
+    if (leftMax > rightMax) {
+        return leftMax;
+    } else {
+        return rightMax;
     }
 }
 
-const numbers = [3, 8, 2, 10, 5, 7];
+const numbers = [3, 8, 2, 10, 5, 7, 12, 1, -4, 88, 0, -90, 100];
+console.log(
+    mergeSort(numbers)
+);
+
+
